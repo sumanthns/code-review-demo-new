977269326a4a9cfdd9294eb68cf882706905682e
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-05-22 21:13:42 +0530

    Cleaning up
    Change-Id: Ia770aa024db3a62a240cf82b758aaea04367a1ec
    Signed-off-by: Sumanth Nagadavalli Suresh <nsready@gmail.com>
    Reviewed-on: https://review.gerrithub.io/362020

--- TestClass.java
+++
@@ -1,5 +0,0 @@
-public class TestClass {
-	public static void main(String... args) {
-		System.out.println("Hi Satish, changes made as directed.");
-	}
-}
--- test.java
+++
@@ -1,3 +0,0 @@
-hi 
-
-i have done  some changes
